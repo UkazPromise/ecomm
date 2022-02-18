d58d101d4a0f630ccba808e47c375fe02155dd56
UkazPromise <ukazpromise@gmail.com>
2022-02-18 19:55:35 +0100

    ecomm
    Material Icons and react

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        Img: "/images/female fashion.png",
+        title: "BONANZA SALE",
+        desc: "DON'T COMPROMISE ON STYLE GET FLAT 30% OF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        Img: "/images/female-fashion-model-png-picture-black-and-white.png",
+        title: "SEASON SALE",
+        desc: "DON'T COMPROMISE ON STYLE GET FLAT 30% OF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        Img: "/images/Phones images.jpg",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE GET FLAT 30% OF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "/images/Overshirt.jpg",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "/images/Sanmsung.jpg",
+        title: "SMART PHONES!",
+    },
+    {
+        id: 3,
+        img: "/images/Lexus LX Premieres.jpg",
+        title: "GOOD CARS!",
+    },
+];
+
+export const popularProducts = [
+    {
+        id:1,
+        img: "/images/Overshirt.jpg",
+    },
+    {
+        id:2,
+        img: "/images/Overshirt.jpg",
+    },
+    {
+        id:3,
+        img: "/images/Overshirt.jpg",
+    },
+    {
+        id:4,
+        img: "/images/Overshirt.jpg",
+    },
+    {
+        id:5,
+        img: "/images/Overshirt.jpg",
+    },
+    {
+        id:6,
+        img: "/images/Overshirt.jpg",
+    },
+    {
+        id:7,
+        img: "/images/Overshirt.jpg",
+    },
+    {
+        id:1,
+        img: "/images/Overshirt.jpg",
+    },
+];
